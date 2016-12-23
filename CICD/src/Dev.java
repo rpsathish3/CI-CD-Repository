db77ad3616b4607ffb758eef1aef7d58de630291
rpsathish3 <sathish.ponnusamy@aig.com>
2016-12-23 16:46:53 -0500

    testing

---
+++ CICD/src/Dev.java
@@ -0,0 +1,9 @@
+
+public class Dev {
+
+	public static void main(String[] args) {
+		System.out.println("Dev");
+
+	}
+
+}
---
+++ CICD/src/QA.java
@@ -0,0 +1,7 @@
+
+public class QA {
+
+	public static void main(String[] args) {
+		System.out.println("QA");
+	}
+}
