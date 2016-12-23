17a0523428293ba6610fddfc6c86d4d6199c1c5c
rpsathish3 <sathish.ponnusamy@aig.com>
2016-12-23 16:52:45 -0500

    test

--- CICD/src/Dev.java
+++ CICD/src/Dev.java
@@ -3,7 +3,5 @@ public class Dev {
 
 	public static void main(String[] args) {
 		System.out.println("Dev");
-
 	}
-
 }
--- CICD/src/QA.java
+++ CICD/src/QA.java
@@ -1,6 +1,5 @@
 
 public class QA {
-
 	public static void main(String[] args) {
 		System.out.println("QA");
 	}
