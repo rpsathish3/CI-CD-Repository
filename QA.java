e4109f83f6b50be879f573cb8a96497136ea7344
rpsathish3 <sathish.ponnusamy@aig.com>
2016-12-23 18:05:55 -0500

    added files thru g

---
+++ QA.java
@@ -0,0 +1,6 @@
+
+public class QA {
+	public static void main(String[] args) {
+		System.out.println("QATest");
+	}
+}
---
+++ run.bat
@@ -0,0 +1,5 @@
+set path = "C:\jdk1.7.0_79\bin";
+
+javac QA.java
+java QA
+pause
