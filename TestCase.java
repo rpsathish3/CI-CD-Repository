6acd8ca6053c19aca84f4f1e79183556c36d61a7
rpsathish3 <sathish.ponnusamy@aig.com>
2016-12-22 20:09:32 -0500

    Update TestCase.java

--- TestCase.java
+++ TestCase.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import junit.framework.testdata.TestData;
 
 public class TestCase {
